668c8a9360eadadb19011b21e82f810116364794
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-24 17:36:33 -0600

    add openstreetmap attribution

--- ISS.js
+++ ISS.js
@@ -1,4 +1,6 @@
 const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+const attribution =
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 async function getISS(){
