0e7deb12ad8a553da1e36efd6b856f716b61c33c
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-24 17:58:06 -0600

    add image of ISS as marker for location using leaflet Icon object

--- ISS.js
+++ ISS.js
@@ -1,5 +1,13 @@
 const mymap = L.map('issMap').setView([0, 0], 1);
-const marker = L.marker([0, 0]).addTo(mymap);
+
+const issIcon = L.icon({
+  iconUrl: 'Space.png',
+  iconSize: [50, 32],
+  iconAnchor: [25, 16]
+});
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(mymap);
+
+
 
 const attribution =
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
@@ -10,7 +18,7 @@ tiles.addTo(mymap);
 
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 async function getISS(){
-  
+
   const response = await fetch(api_url)
   const data = await response.json();
   const {latitude, longitude} = data;
