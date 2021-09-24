464809d2d0ae6b047b5ed7526ff5069a6023ee59
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-24 17:33:24 -0600

    initialize leaflet map

--- ISS.js
+++ ISS.js
@@ -1,3 +1,5 @@
+const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 async function getISS(){
   const response = await fetch(api_url)
