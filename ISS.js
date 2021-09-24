c08234a2eb5ba80b88f0c0c4d07b02ed780f6692
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-24 17:39:32 -0600

    add tiles to map

--- ISS.js
+++ ISS.js
@@ -2,6 +2,10 @@ const mymap = L.map('issMap').setView([51.505, -0.09], 13);
 const attribution =
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';        
+const tiles = L.tileLayer(tileUrl, { attribution });(tileUrl,{attribution})   
+tiles.addTo(mymap);     
+
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
 async function getISS(){
   const response = await fetch(api_url)
