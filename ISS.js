eb26c0c95edf26b0b082070e4fac3373ee17c1f6
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-24 17:40:27 -0600

    set zoom to 1 (no zoom) and centre map

--- ISS.js
+++ ISS.js
@@ -1,4 +1,4 @@
-const mymap = L.map('issMap').setView([51.505, -0.09], 13);
+const mymap = L.map('issMap').setView([0, 0], 1);
 const attribution =
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
