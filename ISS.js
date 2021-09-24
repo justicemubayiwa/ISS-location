bae550eb6ef4693d7018be821e79eee74380ca28
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-09-24 17:02:57 -0600

    re-order operations

--- ISS.js
+++ ISS.js
@@ -1,10 +1,9 @@
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
-
-getISS()
-
 async function getISS(){
   const response = await fetch(api_url)
   const data = await response.json();
   console.log(data)
 }
+
+getISS();
 
